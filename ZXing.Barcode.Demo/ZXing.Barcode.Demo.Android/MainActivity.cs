705c8d85c4277ae0f74df16686c6767279b4ef96
std66 <sinkutamas@gmail.com>
2020-05-01 17:56:27 +0200

    Add project files.

---
+++ ZXing.Barcode.Demo/ZXing.Barcode.Demo.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using ZXing.Mobile;
+
+namespace ZXing.Barcode.Demo.Droid {
+    [Activity(Label = "ZXing.Barcode.Demo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            MobileBarcodeScanner.Initialize(Application);
+
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ ZXing.Barcode.Demo/ZXing.Barcode.Demo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+namespace ZXing.Barcode.Demo {
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage {
+		public MainPage() {
+			InitializeComponent();
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e) {
+			var scanner = new MobileBarcodeScanner();
+
+			var result = await scanner.Scan();
+
+			if (result != null)
+				Barcode.Text = result.Text;
+		}
+	}
+}
